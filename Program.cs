501327b4da9d498d64ebf3d76944da47008a896c
Eszter Digruber <digruber.eszter@verebelyszki.hu>
2023-03-02 14:14:19 +0100

    asd

--- Program.cs
+++ Program.cs
@@ -7,7 +7,22 @@ namespace HelloWorld
         static List<Csoki> Csoki_List = new List<Csoki>();
         static void Main(string[] args)
         {
-            
+            Feladat1Beolvasas(); Console.WriteLine("\n---------------\n");
+        }
+
+        private static void Feladat1Beolvasas()
+        {
+            Console.WriteLine("1.feladat: Beolvasás");
+            var sr = new StreamReader(@"csoki.txt", Encoding.UTF8);
+            int db = 0;
+            while (!sr.EndOfStream)
+            {
+                Csoki_List.Add(new Csoki(sr.ReadLine()));
+                db++;
+            }
+            sr.Close();
+            if (db > 0) { Console.WriteLine("Sikeres a beolvasás"); }
+            else { Console.WriteLine("Sikertelen a beolvasás"); }
         }
     }
 }
