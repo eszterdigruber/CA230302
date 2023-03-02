d4d720e6f38aae98030643aa172d5bda2d45fe89
Eszter Digruber <digruber.eszter@verebelyszki.hu>
2023-03-02 14:05:02 +0100

    asd

---
+++ Csoki.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloWorld
+{
+    internal class Csoki
+    {
+        public string Nev;
+        public string Kod;
+        public int Ar;
+        public int Darabszam;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
-﻿namespace HelloWorld
+﻿using System.IO;
+using System.Text;
+namespace HelloWorld
 {
     class Program
     {
+        static List<Csoki> Csoki_List = new List<Csoki>();
         static void Main(string[] args)
         {
             
