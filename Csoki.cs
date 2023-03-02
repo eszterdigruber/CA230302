97de6725a907df759f9ba2e792cdd7c856d0afa5
Eszter Digruber <digruber.eszter@verebelyszki.hu>
2023-03-02 14:07:45 +0100

    asd

--- Csoki.cs
+++ Csoki.cs
@@ -6,5 +6,14 @@
         public string Kod;
         public int Ar;
         public int Darabszam;
+
+        public Csoki(string Sor)
+        {
+            var dbok = Sor.Split(';');
+            this.Nev = dbok[0];
+            this.Kod = dbok[1];
+            this.Ar = int.Parse(dbok[2]);
+            this.Darabszam = int.Parse(dbok[3]);
+        }
     }
 }
